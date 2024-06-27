084dea415e8625b00c3026d3ca07fca26541225d
cel3ntato <adriano2024celentano@gmail.com>
2024-06-28 01:21:05 +0200

    task-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import Profile from "./Profile/Profile";
 import userData from "../userData.json";
 import FriendList from "./FriendList/FriendList";
 import friends from "../friends.json";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import transactions from "../transactions.json";
 export default function App() {
   return (
     <>
@@ -14,6 +16,7 @@ export default function App() {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 }
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,25 @@
+import css from "./TransactionHistory.module.css";
+export default function TransactionHistory({ items }) {
+  return (
+    <table className={css.transactionTable}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id}>
+              <td className={css.transactionType}>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
